56bae64f07cc5124d70494f0d80ba367f3844c2e
Maverick <sarangsingh29@yahoo.com>
2017-08-24 15:45:02 +0000

    First Commit

---
+++ app/src/main/java/maverick/com/speechcommander/SpeechCommanderControllerActivity.java
@@ -0,0 +1,13 @@
+package maverick.com.speechcommander;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpeechCommanderControllerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_speech_commander_controller);
+    }
+}
